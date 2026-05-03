f0a8591f590109cb6ed2c7cded525f76b3767579
vesselvi <ignacio.caram.43@gmail.com>
2026-05-03 17:49:05 -0300

    Agregue info y estilo al header

---
+++ js/data.js
@@ -0,0 +1,9 @@
+let person = {
+    name= "Ignacio Zottola Caram",
+    age= 27,
+    stack =["C#", ".NET", "Javascript", "Node.js"],
+    dbStack =["MySQL", "MongoDB"],
+    country = "Argentina",
+    province = "Tucuman",
+    university ="Universidad Nacional de Tucuman"
+};
